c5bfb3b4a59bd356be0e19f34e8ac3f3f6151281
waseem abbas <waseemofficee@gmail.com>
2025-03-04 22:24:47 +0500

    bootstrap setup

--- src/Btn.jsx
+++ src/Btn.jsx
@@ -1,5 +1,14 @@
 const Btn = () => {
-    return <button>btn</button>
+    return <>
+        <div class="d-flex gap-2  justify-content-center py-5">
+            <button class="btn btn-primary rounded-pill px-3" type="button">Primary</button>
+            <button class="btn btn-secondary rounded-pill px-3" type="button">Secondary</button>
+            <button class="btn btn-success rounded-pill px-3" type="button">Success</button>
+            <button class="btn btn-danger rounded-pill px-3" type="button">Danger</button>
+            <button class="btn btn-warning rounded-pill px-3" type="button">Warning</button>
+            <button class="btn btn-info rounded-pill px-3" type="button">Info</button>
+        </div>
+    </>
 }
 
 export default Btn;
