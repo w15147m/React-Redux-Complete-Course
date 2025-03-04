b5a5f96adaab3a87e0f131e5f18eedcc3fdd3e8a
waseem abbas <waseemofficee@gmail.com>
2025-03-04 22:26:07 +0500

    update code

--- src/Btn.jsx
+++ src/Btn.jsx
@@ -1,14 +1,14 @@
 const Btn = () => {
-    return <>
-        <div class="d-flex gap-2  justify-content-center py-5">
-            <button class="btn btn-primary rounded-pill px-3" type="button">Primary</button>
-            <button class="btn btn-secondary rounded-pill px-3" type="button">Secondary</button>
-            <button class="btn btn-success rounded-pill px-3" type="button">Success</button>
-            <button class="btn btn-danger rounded-pill px-3" type="button">Danger</button>
-            <button class="btn btn-warning rounded-pill px-3" type="button">Warning</button>
-            <button class="btn btn-info rounded-pill px-3" type="button">Info</button>
-        </div>
-    </>
-}
-
-export default Btn;
+    return (
+      <div className="d-flex gap-2  justify-content-center py-5">
+        <button className="btn btn-primary rounded-pill px-3" type="button">Primary</button>
+        <button className="btn btn-secondary rounded-pill px-3" type="button">Secondary</button>
+        <button className="btn btn-success rounded-pill px-3" type="button">Success</button>
+        <button className="btn btn-danger rounded-pill px-3" type="button">Danger</button>
+        <button className="btn btn-warning rounded-pill px-3" type="button">Warning</button>
+        <button className="btn btn-info rounded-pill px-3" type="button">Info</button>
+      </div>
+    );
+  };
+  
+  export default Btn;
