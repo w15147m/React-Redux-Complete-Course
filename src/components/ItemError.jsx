bc127bbc80fcd2e080231abacb215326694902cc
waseem abbas <waseemofficee@gmail.com>
2025-04-30 07:31:51 +0500

    Remove all project files including package.json, source files, and configuration files

--- src/components/ItemError.jsx
+++
@@ -1,3 +0,0 @@
-const ItemError = ({foodItems}) => {
-    return (foodItems.length < 0 && <h1> food not found </h1>);}
-export default ItemError;
--- src/components/iIem.jsx
+++
@@ -1,4 +0,0 @@
-const Item = ({item}) => {
-    return  <li className="list-group-item">{item}</li>
-}
-export default Item
