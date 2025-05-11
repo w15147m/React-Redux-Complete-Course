083ce2eaa7d9e0b736d6bc7c4a5b01dcf47d9a8a
waseem abbas <waseemofficee@gmail.com>
2025-05-11 15:15:31 +0500

    Remove old codebase including package.json, assets, components, styles, and configuration files.

--- Fragments/src/components/ListGroup.jsx
+++
@@ -1,9 +0,0 @@
-import ListItem from "./ListItem";
-function random({ ListItems }) {
-    return (
-        <ul className="list-group">
-                <ListItem  ListItems={ListItems} />
-        </ul>
-    )
-}
-export default random;
